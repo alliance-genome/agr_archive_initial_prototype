52eb98aea52c2889cdf579aaa952d6f0986509cc
Travis Sheppard <tshepp@stanford.edu>
2016-09-26 15:07:29 -0700

    eslint semi

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,6 @@
-import React, { Component, PropTypes } from 'react'
-import { Link } from 'react-router'
-import style from './style.css'
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import style from './style.css';
 
 class App extends Component {
   render() {
@@ -8,17 +8,17 @@ class App extends Component {
         <div className={style.root}>
             <h1>{'React App'}</h1>
             <ul>
-                <li><Link to="/">{'Home'}</Link></li>
+                <li><Link to='/'>{'Home'}</Link></li>
             </ul>
 
             {this.props.children}
         </div>
-    )
+    );
   }
 }
 
 App.propTypes = {
   children: PropTypes.node
-}
+};
 
-export default App
+export default App;
