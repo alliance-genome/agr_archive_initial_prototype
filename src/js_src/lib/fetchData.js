068d2fbfc28881382e25975bd3a74ed1f324f197
Travis Sheppard <tshepp@stanford.edu>
2016-10-18 13:44:34 -0700

    more cancel and timeout

--- src/js_src/lib/fetchData.js
+++ src/js_src/lib/fetchData.js
@@ -1,12 +1,21 @@
 /*eslint-disable no-undef */
+
+const TIMEOUT = 5000;
+
+let xhrRequest = { readyState: 0 };
 export default function fetchData(_url, options={}) {
   let _type = options.type || 'GET';
   return new Promise(function (resolve, reject) {
+    // abort old request if not complete
+    if (xhrRequest.readyState === 1) {
+      xhrRequest.abort();
+    }
     // *** DEPENDS ON GLOBAL $ because $ can abort ***
-    $.ajax({
+    xhrRequest = $.ajax({
       url : _url,
       type : _type,
       dataType:'json',
+      timeout: TIMEOUT,
       success: data => {
         resolve(data);
       },
