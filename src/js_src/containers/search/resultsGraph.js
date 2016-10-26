98751683c0dab355337608a9ed892e82d22aff7e
Travis Sheppard <tshepp@stanford.edu>
2016-10-25 17:41:24 -0700

    draw basic graph

--- src/js_src/containers/search/resultsGraph.js
+++ src/js_src/containers/search/resultsGraph.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createMemoryHistory } from 'react-router';
+import d3 from 'd3';
 
 import { selectGraphData, selectQueryParams } from '../../selectors/searchSelectors';
 import fetchData from '../../lib/fetchData';
@@ -10,6 +11,8 @@ import { SEARCH_API_ERROR_MESSAGE } from '../../constants';
 
 const BASE_SEARCH_URL = '/api/graph_search';
 const DEFAULT_WIDTH = 600;
+const MAX_HEIGHT = 450;
+const TEXT_OFFSET = 3;
 
 class ResultsGraphComponent extends Component {
   constructor(props) {
@@ -30,6 +33,9 @@ class ResultsGraphComponent extends Component {
     if (prevProps.queryParams !== this.props.queryParams) {
       this.fetchData();
     }
+    if (prevProps.data !== this.props.data) {
+      this.drawGraph();
+    }
   }
 
   calcWidth() {
@@ -37,6 +43,67 @@ class ResultsGraphComponent extends Component {
     this.setState({ width: newWidth });
   }
 
+  getHeight() {
+    return Math.min(this.state.width, MAX_HEIGHT);
+  }
+
+  getLinks() {
+    let nodes = this.props.data.nodes;
+    let edges = this.props.data.edges;
+    function findIndexOfNodeById(id) {
+      let thisNode = nodes.filter( d => d.id === id )[0];
+      return nodes.indexOf(thisNode);
+    }
+    return edges.map( d => {
+      let sourceIndex = findIndexOfNodeById(d.source);
+      let targetIndex = findIndexOfNodeById(d.target);
+      return { source: sourceIndex, target: targetIndex };
+    });
+  }
+
+  // a la http://bl.ocks.org/mbostock/3180395
+  drawGraph() {
+    let width = this.state.width;
+    let height = this.getHeight();
+    let nodes = this.props.data.nodes;
+    let links = this.getLinks();
+    if (!nodes.length || !links.length) return;
+
+    let force = d3.layout.force()
+      .size([width, height]);
+    let context = this.refs.canvas.getContext('2d');
+    force
+      .nodes(nodes)
+      .links(links)
+      .linkDistance(50)
+      .on('tick', tick)
+      .start();
+    // setup font
+    context.font = '14px Lato';
+    function tick() {
+      context.clearRect(0, 0, width, height);
+      // draw links
+      context.strokeStyle = '#ccc';
+      context.beginPath();
+      links.forEach(function(d) {
+        context.moveTo(d.source.x, d.source.y);
+        context.lineTo(d.target.x, d.target.y);
+      });
+      context.stroke();
+      context.fillStyle = 'black';
+      nodes.forEach(function(d) {
+        context.fillText(d.name, d.x + TEXT_OFFSET, d.y - TEXT_OFFSET);
+      });
+      // draw nodes
+      context.fillStyle = 'steelblue';
+      context.beginPath();
+      nodes.forEach(function(d) {
+        context.moveTo(d.x, d.y);
+        context.arc(d.x, d.y, 4.5, 0, 2 * Math.PI);
+      });
+      context.fill();
+    }
+  }
 
   fetchData() {
     let tempHistory = createMemoryHistory('/');
@@ -61,7 +128,7 @@ class ResultsGraphComponent extends Component {
   render() {
     return (
       <div ref='container'>
-        <canvas height={this.state.width} ref='canvas' width={this.state.width} />
+        <canvas height={this.getHeight()} ref='canvas' width={this.state.width} />
       </div>
     );
   }
--- src/server.py
+++ src/server.py
@@ -38,7 +38,7 @@ def graph_search():
         'edges': [
             { 'source': 1, 'target': 2 },
             { 'source': 2, 'target': 3 },
-            { 'source': 3, 'target': 4 }
+            { 'source': 3, 'target': 1 }
         ]
     }
     return jsonify(graph_data)
