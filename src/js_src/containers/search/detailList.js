d6d8b8ff797294a7ee61e65c28348262e8df89a4
Travis Sheppard <tshepp@stanford.edu>
2016-11-01 14:17:43 -0700

    add space to array fields

--- src/js_src/containers/search/detailList.js
+++ src/js_src/containers/search/detailList.js
@@ -3,15 +3,21 @@ import React, { Component } from 'react';
 import style from './style.css';
 import { makeFieldDisplayName } from '../../lib/searchHelpers';
 
+const JOIN_CHAR = ', ';
+
 class DetailList extends Component {
   render() {
     let d = this.props.data;
     let nodes = this.props.fields.map( (field) => {
       let valueNode;
+      let value = d[field];
+      if (Array.isArray(value)) {
+        value = value.join(JOIN_CHAR);
+      }
       if (field === 'species') {
-        valueNode = <span><i dangerouslySetInnerHTML={{ __html: d[field] }} /></span>;
+        valueNode = <span><i dangerouslySetInnerHTML={{ __html: value }} /></span>;
       } else {
-        valueNode = <span dangerouslySetInnerHTML={{ __html: d[field] }} />;
+        valueNode = <span dangerouslySetInnerHTML={{ __html: value }} />;
       }
       return (
         <div className={style.detailLineContainer} key={`srField.${field}`}>
