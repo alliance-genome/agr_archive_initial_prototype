179c48f5cf3a768940fe71a305c9c38ebadc72f7
Pedro Assis <pedroh@stanford.edu>
2016-11-04 11:13:37 -0700

    Refactoring indexing scripts

--- scripts/elastic_search/fly.py
+++ scripts/elastic_search/fly.py
@@ -1,13 +1,22 @@
+from mod import MOD
 import csv
 
 
-class Fly():
+class FlyBase(MOD):
+    species = "Mus musculus"
+
     @staticmethod
     def gene_href(gene_id):
         return "http://flybase.org/reports/" + gene_id + ".html"
 
     @staticmethod
-    def load_genes(genes):
+    def gene_id_from_panther(panther_id):
+        # example: FlyBase=FBgn0053056
+        return panther_id.split("=")[1]
+
+    def load_genes(self):
+        genes = MOD.genes
+
         genes_data_csv_filename = "data/FlyBase_Genes_output_fb_2016_04.tsv"
 
         print("Fetching gene data from FlyBase tsv file...")
@@ -15,13 +24,17 @@ class Fly():
         with open(genes_data_csv_filename, 'rb') as f:
             reader = csv.reader(f, delimiter='\t')
             for row in reader:
+                chromosomes = []
+                if row[5]:
+                    chromosomes = [row[5]]
+
                 genes[row[0]] = {
                     "gene_symbol": row[1],
                     "name": row[1],
                     "description": row[4],
                     "gene_synonyms": map(lambda s: s.strip(), row[9].split(",")),
                     "gene_type": row[3],
-                    "gene_chromosomes": [row[5]],
+                    "gene_chromosomes": chromosomes,
                     "gene_chromosome_starts": row[6],
                     "gene_chromosome_ends": row[7],
                     "gene_chromosome_strand": row[8],
@@ -32,13 +45,15 @@ class Fly():
                     "gene_molecular_function": [],
                     "gene_cellular_component": [],
 
-                    "name_key": row[1],
-                    "href": Fly.gene_href(row[0]),
+                    "homologs": [],
+
+                    "name_key": row[1].lower(),
+                    "id": row[0],
+                    "href": FlyBase.gene_href(row[0]),
                     "category": "gene"
                 }
 
-    @staticmethod
-    def load_go(genes, go):
+    def load_go(self):
         go_data_csv_filename = "data/FlyBase_GO_output_fb_2016_04.tsv"
 
         print("Fetching go data from FlyBase tsv file...")
@@ -47,37 +62,11 @@ class Fly():
             reader = csv.reader(f, delimiter='\t')
 
             for row in reader:
-                if 'GO:' + row[2].lower() in ("GO:0008150", "GO:0003674", "GO:0005575"):
-                    continue
-
                 go_genes = map(lambda s: s.strip(), row[4].split(","))
-
-                go_gene_symbols = []
                 for gene in go_genes:
-                    if gene in genes and genes[gene]["gene_symbol"]:
-                        go_gene_symbols.append(genes[gene]["gene_symbol"].upper())
+                    self.add_go_annotation_to_gene(gene_id=gene, go_id='GO:' + row[2])
 
-                if ('GO:' + row[2]) in go:
-                    go['GO:' + row[2]]["go_genes"] += go_gene_symbols
-                    go['GO:' + row[2]]["go_species"].append("Drosophila melanogaster")
-                else:
-                    go['GO:' + row[2]] = {
-                        "name": row[0],
-                        "go_type": row[3].lower(),
-                        "go_genes": go_gene_symbols,
-                        "go_species": ["Drosophila melanogaster"],
-
-                        "name_key": row[0],
-                        "href": "http://amigo.geneontology.org/amigo/term/GO:" + row[2],
-                        "category": "go"
-                    }
-
-                for gene in go_genes:
-                    if gene in genes:
-                        genes[gene]["gene_" + row[3].lower()].append(row[0])
-
-    @staticmethod
-    def load_diseases(genes, diseases):
+    def load_diseases(self):
         diseases_data_csv_filename = "data/FlyBase_DOID_output_fb_2016_04.tsv"
 
         print("Fetching disease data from FlyBase tsv file...")
@@ -90,22 +79,7 @@ class Fly():
                 if row[3]:
                     omim_ids = map(lambda s: s.strip(), row[3].split(","))
                     disease_gene_ids = map(lambda s: s.strip(), row[5].split(","))
-                    disease_genes = set([])
-                    for gene in disease_gene_ids:
-                        if gene in genes:
-                            disease_genes.add(genes[gene]["gene_symbol"].upper())
 
                     for omim_id in omim_ids:
-                        if omim_id in diseases:
-                            diseases[omim_id]["disease_genes"] += disease_genes
-                            diseases[omim_id]["disease_species"] += ["Drosophila melanogaster"]
-                        else:
-                            diseases[omim_id] = {
-                                "name": row[1],
-                                "disease_genes": list(disease_genes),
-                                "disease_species": ["Drosophila melanogaster"],
-
-                                "name_key": row[1],
-                                "href": "http://omim.org/entry/" + str(omim_id),
-                                "category": "disease"
-                            }
+                        for gene_id in disease_gene_ids:
+                            self.add_disease_annotation_to_gene(gene_id=gene_id, omim_id="OMIM:"+omim_id)
--- scripts/elastic_search/index.py
+++ scripts/elastic_search/index.py
@@ -1,9 +1,61 @@
 from elasticsearch import Elasticsearch
 import os
 import requests
+import pickle
+
+import time
 
 from mapping import mapping
+
+from sgd import SGD
+from zfin import ZFin
+from worm import WormBase
+from fly import FlyBase
+from mouse import MGI
+from rat import RGD
+
 from mod import MOD
+
+sgd = SGD()
+zfin = ZFin()
+worm = WormBase()
+fly = FlyBase()
+mouse = MGI()
+rat = RGD()
+
+mod = MOD()
+
+mods = [mouse, zfin, sgd, worm, fly, rat]
+
+for m in mods:
+    start_time = time.time()
+    m.load_genes()
+    print (" --- %s seconds --- " % (time.time() - start_time))
+
+mod.load_homologs()
+
+for m in mods:
+    start_time = time.time()
+    m.load_go()
+    print (" --- %s seconds --- " % (time.time() - start_time))
+
+for m in mods:
+    start_time = time.time()
+    m.load_diseases()
+    print (" --- %s seconds --- " % (time.time() - start_time))
+
+mod.save_into_file()
+
+mod.delete_mapping()
+mod.put_mapping()
+
+mod.index_genes_into_es()
+mod.index_go_into_es()
+mod.index_diseases_into_es()
+
+import pdb; pdb.set_trace()
+
+
 
 INDEX_NAME = 'searchable_items_blue'
 DOC_TYPE = 'searchable_item'
@@ -30,22 +82,47 @@ def put_mapping():
 genes = {}
 go = {}
 diseases = {}
+homology = {}
 
-species = ("M. musculus", "S. cerevisiae", "D. renio", "C. elegans", "D. melanogaster")
+species = ["M. musculus", "S. cerevisiae", "D. rerio", "C. elegans", "D. melanogaster"]
 
-for specie in species:
-    MOD.factory(specie).load_genes(genes)
 
-for specie in species:
-    MOD.factory(specie).load_diseases(genes, diseases)
+#go_dataset = MOD().load_go()
+#import pdb; pdb.set_trace()
 
-for specie in species:
-    MOD.factory(specie).load_go(genes, go)
+mod = MOD()
 
-delete_mapping()
-put_mapping()
+if os.path.isfile(gene_bkp_filename):
+    with open(gene_bkp_filename, "rb") as f:
+        print "Loading all gene data from backup file..."
+        mod.genes = pickle.load(f)
+else:
+    for s in species:
+        MOD.factory(s).load_genes(mod.genes)
 
-datatypes = [diseases, go, genes]
+MOD.load_homologs(mod.genes)
+
+if os.path.isfile(go_bkp_filename):
+    with open(go_bkp_filename, "rb") as f:
+        print "Loading all go data from backup file..."
+        go = pickle.load(f)
+else:
+    for s in species:
+        MOD.factory(s).load_go(genes, go)
+
+if os.path.isfile(diseases_bkp_filename):
+    with open(diseases_bkp_filename, "rb") as f:
+        print "Loading all diseases data from backup file..."
+        diseases = pickle.load(f)
+else:
+    for s in species:
+        MOD.factory(s).load_diseases(genes, diseases)
+
+#delete_mapping()
+#put_mapping()
+
+#datatypes = [genes, diseases, go]
+datatypes = [genes]
 
 print "Indexing ElasticSearch..."
 
@@ -68,3 +145,19 @@ for datatype in datatypes:
 
     if len(bulk_data) > 0:
         es.bulk(index=INDEX_NAME, body=bulk_data, refresh=True)
+
+
+if not os.path.isfile(gene_bkp_filename):
+    with open(gene_bkp_filename, "wb") as f:
+        print "Saving gene data into a backup file..."
+        pickle.dump(genes, f, pickle.HIGHEST_PROTOCOL)
+
+if not os.path.isfile(go_bkp_filename):
+    with open(go_bkp_filename, "wb") as f:
+        print "Saving go data into a backup file..."
+        pickle.dump(go, f, pickle.HIGHEST_PROTOCOL)
+
+if not os.path.isfile(diseases_bkp_filename):
+    with open(diseases_bkp_filename, "wb") as f:
+        print "Saving diseases data into a backup file..."
+        pickle.dump(diseases, f, pickle.HIGHEST_PROTOCOL)
