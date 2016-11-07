7558fc60572f22510ce9dc0ff18722e7b159fef3
Travis Sheppard <tshepp@stanford.edu>
2016-11-07 15:10:53 -0800

    render with sigma

--- src/js_src/components/graph.js
+++ src/js_src/components/graph.js
@@ -1,25 +1,11 @@
 /*eslint-disable react/no-set-state */
 import React, { Component } from 'react';
-import d3 from 'd3';
-
 import getSpeciesColorScale from '../lib/getSpeciesColorScale';
 
-const DEFAULT_WIDTH = 600;
-const MAX_HEIGHT = 750;
+const MAX_HEIGHT = 350;
+const TARGET_ID = 'j-agr-sigma-target';
 
 class Graph extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: DEFAULT_WIDTH
-    };
-  }
-
-  // fetch data at start
-  componentDidMount() {
-    this.calcWidth();
-  }
-
   // fetch data whenever URL changes within /search
   componentDidUpdate (prevProps) {
     if (prevProps.data !== this.props.data) {
@@ -27,86 +13,60 @@ class Graph extends Component {
     }
   }
 
-  calcWidth() {
-    let newWidth = this.refs.container.getBoundingClientRect().width;
-    this.setState({ width: newWidth });
-  }
-
   getHeight() {
-    return Math.min(this.state.width, MAX_HEIGHT);
+    return MAX_HEIGHT;
   }
 
-  getLinks() {
-    let nodes = this.props.data.nodes;
-    let edges = this.props.data.edges;
-    function findIndexOfNodeById(id) {
-      let thisNode = nodes.filter( d => d.id === id )[0];
-      return nodes.indexOf(thisNode);
-    }
-    return edges.map( d => {
-      let sourceIndex = findIndexOfNodeById(d.source);
-      let targetIndex = findIndexOfNodeById(d.target);
-      return { source: sourceIndex, target: targetIndex };
+  getEdges() {
+    let data = this.props.data;
+    let rawEdges = data.edges;
+    return rawEdges.map( (d, i) => {
+      d.id = `e${i}`;
+      d.color = '#e2e2e2';
+      return d;
     });
   }
 
   getNodes() {
     let colorScale = getSpeciesColorScale();
-    return this.props.data.nodes.map( d => {
+    let length = this.props.data.nodes.length;
+    return this.props.data.nodes.map( (d, i) => {
       d.color = colorScale(d.species);
+      d.label = d.name;
+      d.x = Math.cos(Math.PI * 2 * i / length);
+      d.y = Math.sin(Math.PI * 2 * i / length);
+      d.size = d.direct ? 1 : 0.75;
       return d;
     });
   }
 
-  // a la http://bl.ocks.org/mbostock/3180395
   drawGraph() {
-    let width = this.state.width;
-    let height = this.getHeight();
-    let nodes = this.getNodes();
-    let links = this.getLinks();
-    if (!nodes.length || !links.length) return;
-
-    let force = d3.layout.force()
-      .size([width, height]);
-    let context = this.refs.canvas.getContext('2d');
-    force
-      .nodes(nodes)
-      .links(links)
-      .linkDistance(20)
-      .on('tick', tick)
-      .start();
-    // setup font
-    context.font = '14px Lato';
-    function tick() {
-      context.clearRect(0, 0, width, height);
-      // draw links
-      context.strokeStyle = '#ccc';
-      context.beginPath();
-      links.forEach(function(d) {
-        context.moveTo(d.source.x, d.source.y);
-        context.lineTo(d.target.x, d.target.y);
-      });
-      context.stroke();
-      // context.fillStyle = 'black';
-      // nodes.forEach(function(d) {
-      //   context.fillText(d.name, d.x + TEXT_OFFSET, d.y - TEXT_OFFSET);
-      // });
-      // draw nodes
-      nodes.forEach(function(d) {
-        context.beginPath();
-        context.fillStyle = d.color;
-        context.moveTo(d.x, d.y);
-        context.arc(d.x, d.y, 4.5, 0, 2 * Math.PI);
-        context.fill();
-      });
-      
+    let _nodes = this.getNodes();
+    if (!_nodes.length) return;
+    let _graph = {
+      nodes: _nodes,
+      edges: this.getEdges()
+    };
+    if (this.s) {
+      this.s.graph.clear();
+      this.s.refresh();
     }
+    this.s = new sigma({
+      graph: _graph,
+      container: TARGET_ID,
+      settings: {
+        labelThreshold: 100,
+        minNodeSize: 0,
+        maxNodeSize: 3,
+      }
+    });
+    this.s.startForceAtlas2();
   }
 
   render() {
     return (
       <div ref='container'>
-        <canvas height={this.getHeight()} ref='canvas' width={this.state.width} />
+        <div id={TARGET_ID} style={{ height: this.getHeight() }} />
       </div>
     );
   }
