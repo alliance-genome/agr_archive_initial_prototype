58f808c3ca00454f1081d9f2fdae8ddfe06bd849
Sierra Moxon <staylor1@uoregon.edu>
2017-02-09 12:23:15 -0800

    attempt to migrate load_genes into mod.py

--- scripts/elastic_search/mod.py
+++ scripts/elastic_search/mod.py
@@ -4,6 +4,7 @@ import requests
 import os
 import re
 import json
+import fnmatch
 from elasticsearch import Elasticsearch
 
 class MOD():
@@ -55,99 +56,100 @@ class MOD():
     def _load_genes(self):
         path = "data/"
         for full_data_file_path in os.listdir(path):
-            with open(full_data_file_path) as data_file:
-                data_content = json.load(data_file)
+            if fnmatch.fnmatch(file, '*.json'):
+                with open(full_data_file_path) as data_file:
+                    data_content = json.load(data_file)
 
-            for geneRecord in data_content['data']:
-                synonyms = []
-                crossReferences = {}
-                description = None
-                external_ids = []
-                gene_chromosomes = []
-                gene_chromosome_starts = []
-                gene_chromosome_ends = []
-                gene_chromosome_strands = []
-                gene_chromosome_assemblies = []
-                genomicLocations = {}
-                chromosome = None
-                start = None
-                end = None
-                strand = None
-                assembly = None
-                secondaryIds = []
-                geneSynopsis = None
-                geneSynopsisUrl = None
-                geneLiteratureUrl = None
+                for geneRecord in data_content['data']:
+                    synonyms = []
+                    crossReferences = {}
+                    description = None
+                    external_ids = []
+                    gene_chromosomes = []
+                    gene_chromosome_starts = []
+                    gene_chromosome_ends = []
+                    gene_chromosome_strands = []
+                    gene_chromosome_assemblies = []
+                    genomicLocations = {}
+                    chromosome = None
+                    start = None
+                    end = None
+                    strand = None
+                    assembly = None
+                    secondaryIds = []
+                    geneSynopsis = None
+                    geneSynopsisUrl = None
+                    geneLiteratureUrl = None
 
-                if 'synonyms' in geneRecord:
-                    for synonym in geneRecord['synonyms']:
-                        synonyms.append(synonym)
-                if 'description' in geneRecord:
-                    description = geneRecord['description']
-                if 'crossReferences' in geneRecord:
-                    for crossRef in geneRecord['crossReferences']:
-                        refText = crossRef['dataProvider'] + " " + crossRef['id']
-                        external_ids.append(refText)
-                        crossReferences = {"dataProvider": crossRef['dataProvider'], "id": crossRef['id']}
-                if 'genomeLocations' in geneRecord:
-                    for genomeLocation in geneRecord['genomeLocations']:
-                        gene_chromosomes.append(genomeLocation['chromosome'])
-                        chromosome = genomeLocation['chromosome']
-                        gene_chromosome_assemblies.append(genomeLocation['assembly'])
-                        assembly = genomeLocation['assembly']
-                        if 'start' in genomeLocation:
-                            gene_chromosome_starts.append(genomeLocation['start'])
-                            start = genomeLocation['start']
-                        if 'end' in geneRecord['genomeLocations']:
-                            gene_chromosome_ends.append(genomeLocation['end'])
-                            end = genomeLocation['end']
-                        if 'strand' in geneRecord['genomeLocations']:
-                            gene_chromosome_strands.append(genomeLocation['strand'])
-                            strand = genomeLocation['strand']
-                    genomicLocations = {"chromosome": chromosome, "start": start, "end": end, "strand": strand,
-                                    "assembly": assembly}
-                if 'geneLiteratureUrl' in geneRecord:
-                    geneLiteratureUrl = geneRecord['geneLiteratureUrl']
-                if 'secondaryIds' in geneRecord:
-                    for secondaryId in geneRecord['secondaryIds']:
-                        secondaryIds.append(secondaryId)
-                if 'geneSynopsis' in geneRecord:
-                    geneSynopsis = geneRecord['geneSynopsis']
-                if 'geneSynopsisUrl' in geneRecord:
-                    geneSynopsisUrl = geneRecord['geneSynopsisUrl']
+                    if 'synonyms' in geneRecord:
+                        for synonym in geneRecord['synonyms']:
+                            synonyms.append(synonym)
+                    if 'description' in geneRecord:
+                        description = geneRecord['description']
+                    if 'crossReferences' in geneRecord:
+                        for crossRef in geneRecord['crossReferences']:
+                            refText = crossRef['dataProvider'] + " " + crossRef['id']
+                            external_ids.append(refText)
+                            crossReferences = {"dataProvider": crossRef['dataProvider'], "id": crossRef['id']}
+                    if 'genomeLocations' in geneRecord:
+                        for genomeLocation in geneRecord['genomeLocations']:
+                            gene_chromosomes.append(genomeLocation['chromosome'])
+                            chromosome = genomeLocation['chromosome']
+                            gene_chromosome_assemblies.append(genomeLocation['assembly'])
+                            assembly = genomeLocation['assembly']
+                            if 'start' in genomeLocation:
+                                gene_chromosome_starts.append(genomeLocation['start'])
+                                start = genomeLocation['start']
+                            if 'end' in geneRecord['genomeLocations']:
+                                gene_chromosome_ends.append(genomeLocation['end'])
+                                end = genomeLocation['end']
+                            if 'strand' in geneRecord['genomeLocations']:
+                                gene_chromosome_strands.append(genomeLocation['strand'])
+                                strand = genomeLocation['strand']
+                         genomicLocations = {"chromosome": chromosome, "start": start, "end": end, "strand": strand,
+                                        "assembly": assembly}
+                    if 'geneLiteratureUrl' in geneRecord:
+                        geneLiteratureUrl = geneRecord['geneLiteratureUrl']
+                    if 'secondaryIds' in geneRecord:
+                        for secondaryId in geneRecord['secondaryIds']:
+                            secondaryIds.append(secondaryId)
+                    if 'geneSynopsis' in geneRecord:
+                        geneSynopsis = geneRecord['geneSynopsis']
+                    if 'geneSynopsisUrl' in geneRecord:
+                        geneSynopsisUrl = geneRecord['geneSynopsisUrl']
 
-                # TODO: maybe this method can be generic - running thru the dictionary and adding key:value pairs based on the JSON object now that the mapping.py matches the JSON schema.
+                    # TODO: maybe this method can be generic - running thru the dictionary and adding key:value pairs based on the JSON object now that the mapping.py matches the JSON schema.
 
-                self.genes[geneRecord['primaryId']] = {
-                    "symbol": geneRecord['symbol'],
-                    "name": geneRecord['name'],
-                    "description": description,
-                    "synonyms": synonyms,
-                    "soTermId": geneRecord['soTermId'],
-                    "soTermName": None,
-                    "secondaryIds": secondaryIds,
-                    "geneSynopsis": geneSynopsis,
-                    "geneSynopsisUrl": geneSynopsisUrl,
-                    "gene_chromosomes": gene_chromosomes,
-                    "gene_chromosome_starts": gene_chromosome_starts,
-                    "gene_chromosome_ends": gene_chromosome_ends,
-                    "gene_chromosome_strand": gene_chromosome_strands,
-                    "taxonId": geneRecord['taxonId'],
-                    "species": self.getSpecies(geneRecord['taxonId']),
-                    "external_ids": external_ids,
-                    "gene_biological_process": [],
-                    "gene_molecular_function": [],
-                    "gene_cellular_component": [],
-                    "genomeLocations": genomicLocations,
-                    "homologs": [],
-                    "geneLiteratureUrl": geneLiteratureUrl,
-                    "name_key": geneRecord['name'].lower(),
-                    "primaryId": geneRecord['primaryId'],
-                    "crossReferences": crossReferences,
-                    "href": None,
-                    "category": "gene"
-                }
-            full_data_file_path.close()
+                    self.genes[geneRecord['primaryId']] = {
+                        "symbol": geneRecord['symbol'],
+                        "name": geneRecord['name'],
+                        "description": description,
+                        "synonyms": synonyms,
+                        "soTermId": geneRecord['soTermId'],
+                        "soTermName": None,
+                        "secondaryIds": secondaryIds,
+                        "geneSynopsis": geneSynopsis,
+                        "geneSynopsisUrl": geneSynopsisUrl,
+                        "gene_chromosomes": gene_chromosomes,
+                        "gene_chromosome_starts": gene_chromosome_starts,
+                        "gene_chromosome_ends": gene_chromosome_ends,
+                        "gene_chromosome_strand": gene_chromosome_strands,
+                        "taxonId": geneRecord['taxonId'],
+                        "species": self.getSpecies(geneRecord['taxonId']),
+                        "external_ids": external_ids,
+                        "gene_biological_process": [],
+                        "gene_molecular_function": [],
+                        "gene_cellular_component": [],
+                        "genomeLocations": genomicLocations,
+                        "homologs": [],
+                        "geneLiteratureUrl": geneLiteratureUrl,
+                        "name_key": geneRecord['name'].lower(),
+                        "primaryId": geneRecord['primaryId'],
+                        "crossReferences": crossReferences,
+                        "href": None,
+                        "category": "gene"
+                    }
+                full_data_file_path.close()
 
 
     @staticmethod
