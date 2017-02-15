5810dac1e7ea9f0d4a3e492f1167720fca7c98e1
Patrick Kalita <patrick.kalita@gmail.com>
2017-02-15 10:34:41 -0800

    Pull basic gene info data from backend; link to gene page in search results

--- src/js_src/actions/genes.js
+++ src/js_src/actions/genes.js
@@ -1,26 +1,23 @@
-import fetchData from '../lib/fetchData';
-
 export const FETCH_GENE = 'FETCH_GENE';
 export const FETCH_GENE_SUCCESS = 'FETCH_GENE_SUCCESS';
 export const FETCH_GENE_FAILURE = 'FETCH_GENE_FAILURE';
 
-export function fetchGene(id) {
+export const fetchGene = function () {
   return {
     type: FETCH_GENE,
-    payload: fetchData('/api/gene/' + id),
   };
-}
+};
 
-export function fetchGeneSuccess(gene) {
+export const fetchGeneSuccess = function (gene) {
   return {
     type: FETCH_GENE_SUCCESS,
     payload: gene,
   };
-}
+};
 
-export function fetchGEneFailure(error) {
+export const fetchGeneFailure = function (error) {
   return {
     type: FETCH_GENE_FAILURE,
     payload: error,
   };
-}
+};
