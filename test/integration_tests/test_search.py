3cf731e5472520073be45e139a73e829c313d513
Kevin Schaper <kevinschaper@gmail.com>
2017-02-23 12:37:49 -0800

    Show gene type in results, use 'Biotype' label to match the gene page

--- test/integration_tests/test_search.py
+++ test/integration_tests/test_search.py
@@ -71,9 +71,9 @@ class SearchEndpointsTest(unittest.TestCase):
         }
         self.index = 'searchable_items_blue'
         self.search_fields = ['id', 'name', 'symbol', 'synonyms', 'description', 'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes', 'disease_synonyms', 'homologs.symbol', 'homologs.panther_family']
-        self.json_response_fields = ['name', 'symbol', 'synonyms', 'gene_type', 'gene_chromosomes','gene_chromosome_starts', 'gene_chromosome_ends', 'description', 'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes', 'disease_synonyms', 'homologs', 'crossReferences', 'category', 'href']
+        self.json_response_fields = ['name', 'symbol', 'synonyms', 'soTermName', 'gene_chromosomes','gene_chromosome_starts', 'gene_chromosome_ends', 'description', 'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes', 'disease_synonyms', 'homologs', 'crossReferences', 'category', 'href']
         self.category_filters = {
-            "gene": ['gene_type', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'species'],
+            "gene": ['soTermName', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'species'],
             "go": ['go_type', 'go_species', 'go_genes'],
             "disease": ['disease_species', 'disease_genes']
         }
