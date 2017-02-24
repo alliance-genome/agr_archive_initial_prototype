deaa204502c01ce8cd8566fbadc4e133248b4888
Olin Blodgett <nilo@nilosplace.net>
2017-02-24 06:23:23 -0500

    Added first test model

---
+++ src/models/category.py
@@ -0,0 +1,11 @@
+from server import db
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
