81bad4481d97697e026e926e0f8760550608a3ec
Kevin Schaper <kevinschaper@gmail.com>
2017-03-15 12:26:59 -0700

    updated search_fields collection in tests to match search_service

--- test/integration_tests/test_search.py
+++ test/integration_tests/test_search.py
@@ -70,7 +70,10 @@ class SearchEndpointsTest(unittest.TestCase):
             }
         }
         self.index = 'searchable_items_blue'
-        self.search_fields = ['id', 'name', 'symbol', 'synonyms', 'description', 'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes', 'disease_synonyms', 'homologs.symbol', 'homologs.panther_family']
+        self.search_fields = ['primaryId', 'name', 'symbol', 'symbol.raw', 'synonyms', 'synonyms.raw', 'description',
+                         'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function',
+                         'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes',
+                         'disease_synonyms']
         self.json_response_fields = ['name', 'symbol', 'synonyms', 'soTermName', 'gene_chromosomes','gene_chromosome_starts', 'gene_chromosome_ends', 'description', 'external_ids', 'species', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'go_type', 'go_genes', 'go_synonyms', 'disease_genes', 'disease_synonyms', 'homologs', 'crossReferences', 'category', 'href']
         self.category_filters = {
             "gene": ['soTermName', 'gene_biological_process', 'gene_molecular_function', 'gene_cellular_component', 'species'],
