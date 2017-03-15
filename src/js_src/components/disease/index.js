6ac77e5ee44b8dab4698a5ce9e0f412963c26adc
Bryon Czoch <bczoch@iu.edu>
2017-03-15 14:53:51 -0400

    removed style prop

--- src/js_src/components/disease/index.js
+++ src/js_src/components/disease/index.js
@@ -1,63 +1,19 @@
 import React, { Component } from 'react';
 import FlybaseDataGrid from 'react-flybase-datagrid';
 import faker from 'faker';
-//import './agr.css';
-import {StyleSheet, css} from 'aphrodite';
-
-const styles = StyleSheet.create({
- column: {
-  backgroundColor: '#000',
- },
- field_label: {
-  backgroundColor: '#000',
-  borderColor: '#000000',
-  borderRight: '1px',
-  borderBottom: '1px',
-  borderTop: '0px',
-  fontWeight: 'bold',
-  wordWrap: 'break-word',
-
-  height: '100%',
-  width: '100%',
- },
- wrapperStyles: {
-    // marginTop: '1rem',
-    // marginLeft: '1rem',
-    // marginRight: '3rem',
-    // border: 'none',
-    // overflow:'hidden',
-    // height: '100%',
-    // borderBottom: "1px solid #000000"
-  },
-  newTableHeader: {
-    // color: '#000',
-    // fontSize: '12px',
-    // lineHeight: '1',
-    // background: '#FFFFFF',
-    // border: 'none',
-
-  },
-  newCellBorder: {
-    // borderBottomStyle: 'solid',
-    // borderBottomWidth: '1px',
-    // borderBottom: '1px solid #000000',
-    backgroundColor: '#ffffff',
-    border: '1px',
-  }
-});
-
+import './agr.css';
 
 function getHeaders(){
 
- var columns = [
+  var columns = [
    {id:'id', name:'ID'},
    {id:'name', name:'Name'},
    {id:'address', name:'Street Address'},
    {id:'state', name:'State'},
    {id:'zip', name:'Zip Code'}
- ];
+  ];
 
- return columns;
+  return columns;
 
 }
 
@@ -65,11 +21,11 @@ function generateList(){
   var items = [];
 
   for (var i=1; i<=5000; i++){
-   items.push({ id: i, name: faker.name.findName(), address: faker.address.streetAddress(), state: faker.address.stateAbbr(), zip: faker.address.zipCode()});
- }
+    items.push({ id: i, name: faker.name.findName(), address: faker.address.streetAddress(), state: faker.address.stateAbbr(), zip: faker.address.zipCode()});
+  }
 
- return items;
-};
+  return items;
+}
 
 const data = generateList();
 
@@ -77,7 +33,7 @@ class DiseaseTable extends Component {
   render() {
     return (
 
-       <FlybaseDataGrid data={data} columns={getHeaders()} showDownloadButton={false} showFilter={false} style={styles} />
+       <FlybaseDataGrid columns={getHeaders()} data={data} showDownloadButton={false} showFilter={false} />
 
     );
   }
