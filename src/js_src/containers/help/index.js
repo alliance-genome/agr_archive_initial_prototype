6bfa6a0266c9ef900804b25e9bdacf0f9d6f2ebd
LucyHut <lucie.hutchins@yahoo.com>
2017-03-22 13:37:13 -0400

    Added react-helmet support to update the Help/page head

--- src/js_src/containers/help/index.js
+++ src/js_src/containers/help/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 class Help extends Component {
   render() {
     return (
       <div>
+        <Helmet
+           title="Help Using Alliance of Genome Resources "
+           meta={[
+               {property: 'og:title', content: 'Help'},
+           ]} />
         <h1>Help</h1>
       </div>
     );
