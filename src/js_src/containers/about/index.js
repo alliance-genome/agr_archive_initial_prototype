799801d0e90c956139f35c9d2a4b9e1c7c9910e4
LucyHut <lucie.hutchins@yahoo.com>
2017-03-22 13:28:46 -0400

    Added react-helmet support to update the About/page head content

--- src/js_src/containers/about/index.js
+++ src/js_src/containers/about/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 class About extends Component {
   render() {
     return (
       <div>
+        <Helmet
+           title="About Us - Alliance of Genome Resources"
+           meta={[
+               {property: 'og:title', content: 'About'},
+           ]} />
         <h1>About</h1>
       </div>
     );
