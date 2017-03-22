0517ed68e69f401ed4bbf93c9ef076c24b46a1e9
LucyHut <lucie.hutchins@yahoo.com>
2017-03-22 13:29:55 -0400

    Added react-helmet support to update the Home/page head content

--- src/js_src/containers/home/index.js
+++ src/js_src/containers/home/index.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Helmet
+           title="Home - Alliance of Genome Resources"
+           meta={[
+               {property: 'og:title', content: 'Home'},
+           ]} />
         <h1>Home</h1>
       </div>
     );
