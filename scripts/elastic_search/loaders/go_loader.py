9277ea6e52c7775bf03e3f2750ab1d735fade014
Chris Tabone <ctabone@morgan.harvard.edu>
2017-03-23 10:29:14 -0400

    AGR-12 #comment Work in progress commit. Separated loaders and several small changes.

--- scripts/elastic_search/loaders/go_loader.py
+++ scripts/elastic_search/loaders/go_loader.py
@@ -5,15 +5,15 @@ import re
 class GoLoader:
 
     def __init__(self):
-        path = "tmp";
-        S3File("mod-datadumps/data", "go.obo", path).download()
-        self.go_data = TXTFile(path + "/go.obo").get_data()
+        self.path = "tmp";
+        S3File("mod-datadumps/data", "go.obo", self.path).download()
 
     def get_data(self):
+        go_data = TXTFile(self.path + "/go.obo").get_data()
         go_dataset = {}
         creating_term = None
 
-        for line in self.go_data:
+        for line in go_data:
             line = line.strip()
 
             if line == "[Term]":
@@ -40,3 +40,33 @@ class GoLoader:
                     else:
                         go_dataset[creating_term][key] = [value]
         return go_dataset
+
+    def process_data(self):
+
+            self.go[go_id] = {
+                "go_genes": [gene_symbol],
+                "go_species": [species],
+
+                "name": self.go_dataset[go_id]["name"][0],
+                "description": self.go_dataset[go_id]["def"][0],
+                "go_type": self.go_dataset[go_id]["namespace"][0],
+                "go_synonyms": self.go_dataset[go_id].get("synonym"),
+
+                "name_key": self.go_dataset[go_id]["name"][0],
+                "id": go_id,
+                "href": "http://amigo.geneontology.org/amigo/term/" + go_id,
+                "category": "go"
+            }
+
+    # def process_go_data():
+    #     if species == "Danio rerio":
+    #         gene_symbol = self.genes[gene_id]["symbol"]
+    #     else:
+    #         gene_symbol = self.genes[gene_id]["symbol"].upper()   
+
+    #     if go_id in self.go:
+    #         if gene_symbol not in self.go[go_id]["go_genes"]:
+    #             self.go[go_id]["go_genes"].append(gene_symbol)
+    #         if species not in self.go[go_id]["go_species"]:
+    #             self.go[go_id]["go_species"].append(species)
+    #     else:
