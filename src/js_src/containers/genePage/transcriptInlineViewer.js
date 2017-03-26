4b9c903453b3d7549dc15456d7205ec085306cda
Nathan Dunn <ndunn@me.com>
2017-03-25 23:27:13 -0700

    added two links

--- src/js_src/containers/genePage/transcriptInlineViewer.js
+++ src/js_src/containers/genePage/transcriptInlineViewer.js
@@ -21,8 +21,8 @@ class TranscriptViewer extends Component {
   }
 
   render() {
-    // let jbrowsePrefx = 'http://bw.scottcain.net/jbrowse/?data=data%2F';
-    let jbrowsePrefx = 'http://localhost/jbrowse/overview.html?data=data%2F';
+    let externalPrefx = 'http://bw.scottcain.net/jbrowse/?data=data%2F';
+    let internalPrefix = 'http://localhost/jbrowse/overview.html?data=data%2F';
     let visualizationUrl = 'http://dev.alliancegenome.org:8891/?url=';
     let delay = 5000;
     let pngSuffix = '&format=PNG&delay=' + delay + '&width=600&height=300&zoom=1&quality=0.7';
@@ -32,12 +32,17 @@ class TranscriptViewer extends Component {
     let fmax = this.props.fmax ? this.props.fmax : 20000;
     let locationString = this.props.chromosome + ':' + fmin + '..' + fmax;
 
-    let jbrowseUrl = jbrowsePrefx + encodeURI(this.props.species) + '&loc=' + encodeURI(locationString) + '&tracks=All%20Genes&highlight=';
-    let finalUrl = visualizationUrl + encodeURIComponent(jbrowseUrl.replace('DNA%2C', '')) + pngSuffix;
+    let internalJbrowseUrl = internalPrefix + encodeURI(this.props.species) + '&loc=' + encodeURI(locationString) + '&tracks=All%20Genes&highlight=';
+    let externalJbrowseUrl = externalPrefx + encodeURI(this.props.species) + '&loc=' + encodeURI(locationString) + '&tracks=All%20Genes&highlight=';
+
+    // TODO: move EVERYTHING to the externalJBrowseUrl
+    let finalUrl = visualizationUrl + encodeURIComponent(internalJbrowseUrl.replace('DNA%2C', '')) + pngSuffix;
 
     return (
       <div className={style.jbrowse}>
-        <iframe id="genomeFrame" className={style.jbrowse} src={jbrowseUrl}/>
+        {/*<a href={externalJbrowseUrl}>*/}
+        <iframe id="genomeFrame" className={style.jbrowse} src={internalJbrowseUrl}  />
+        {/*</a>*/}
         {/*<a href={jbrowseUrl} rel="noopener noreferrer" target='_blank'>*/}
           {/*<img*/}
             {/*onError={this.handleImageErrored.bind(this)}*/}
@@ -50,6 +55,7 @@ class TranscriptViewer extends Component {
           {/*: ''*/}
         {/*}*/}
       </div>
+
     );
   }
 
