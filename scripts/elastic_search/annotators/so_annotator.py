57d27cfe73a3e214d977276ef8c0c4624b80b69f
Chris Tabone <ctabone@morgan.harvard.edu>
2017-03-27 16:58:53 -0400

    AGR-12 #comment Minor soTermName bug.

--- scripts/elastic_search/annotators/so_annotator.py
+++ scripts/elastic_search/annotators/so_annotator.py
@@ -2,4 +2,4 @@ class SoAnnotator:
 
     @staticmethod
     def attach_annotations(gene, so_dataset):
-        gene['SoTermName'] = so_dataset[gene['soTermId']]['name'][0]
+        gene['soTermName'] = so_dataset[gene['soTermId']]['name'][0]
