dc8967b3dd69cf31e325b4182c4bb35280ee623f
Nathan Dunn <ndunn@me.com>
2017-03-28 11:38:29 -0700

    removed cruft and using specific URLs until the others are found

--- src/js_src/containers/genePage/transcriptInlineViewer.js
+++ src/js_src/containers/genePage/transcriptInlineViewer.js
@@ -19,35 +19,33 @@ class TranscriptViewer extends Component {
 
   render() {
     // let externalPrefix = 'http://bw.scottcain.net/jbrowse/?data=data%2F';
-    // let externalPrefix = 'http://34.208.22.23/jbrowse/overview.html?data=data%2F';
-    let externalPrefix = 'http://52.43.125.139/jbrowse/overview.html?data=data%2F';
-    // let internalPrefix = 'http://localhost/jbrowse/overview.html?data=data%2F';
+    // let externalPrefix = 'http://ec2-34-208-22-23.us-west-2.compute.amazonaws.com/jbrowse/overview.html?data=data%2F';
+    // let externalPrefix = 'http://localhost/jbrowse/overview.html?data=data%2F';
+    // let externalPrefix = 'http://jbrowse.alliancegenome.org/jbrowse/overview.html?data=data%2F';
+    let externalPrefix = 'http://ec2-52-43-125-139.us-west-2.compute.amazonaws.com/jbrowse/overview.html?data=data%2F';
+
+
+    // let visualizationPrefix = 'http://ec2-34-208-22-23.us-west-2.compute.amazonaws.com:8080/?url=';
     // let visualizationPrefix = 'http://dev.alliancegenome.org:8891/?url=';
     // let visualizationPrefix = 'http://localhost:8891/?url=';
-    let visualizationPrefix = 'http://52.43.125.139:8891/?url=';
-    // let visualizationPrefix = 'http://34.208.22.23:8080/?url=';
-    // let visualizationPrefix = 'http://34.208.101.78:8080/?url=';
+    // let visualizationPrefix = 'http://jbrowse.alliancegenome.org:8891/?url=';
+    let visualizationPrefix = 'http://ec2-52-43-125-139.us-west-2.compute.amazonaws.com:8891/?url=';
+
     let delay = 10000;
     let visualizationSuffix = '&format=PNG&delay=' + delay + '&width=800&height=1000&zoom=1&quality=0.7&cors=true';
     // location based data
-    // let locationString = this.props.fmin && this.props.fmax ? this.props.chromosome + ':' + this.props.fmin + '..' + this.props.fmax : this.props.geneSymbol;
     let locationString = this.props.chromosome + ':' + this.props.fmin + '..' + this.props.fmax;
     let uniqueLocation = encodeURI(this.props.species) + '&loc=' + encodeURI(locationString);
 
     let geneSymbolUrl = '&lookupSymbol=' + this.props.geneSymbol;
-    // let internalJbrowseUrl = internalPrefix + uniqueLocation + '&tracks=All%20Genes&highlight=' + geneSymbolUrl;
     let externalJbrowseUrl = externalPrefix + uniqueLocation + '&tracks=All%20Genes&highlight=' + geneSymbolUrl;
 
     // TODO: move EVERYTHING to the externalJBrowseUrl
-    // let finalUrl = visualizationUrl + encodeURIComponent(externalJbrowseUrl.replace('DNA%2C', '')) + pngSuffix;
-    // let finalUrl = visualizationUrl + encodeURIComponent(internalJbrowseUrl.replace('DNA%2C', '')) ;
-    // let visualizationUrl = visualizationPrefix.replace('@IMAGEID@', 'snapshots/' + encodeURI(this.props.species) +'/' + encodeURI(locationString)+ '.jpeg') + uniqueLocation + '&tracks=All%20Genes&highlight=';
     let visualizationUrl = visualizationPrefix + encodeURIComponent(externalJbrowseUrl) + visualizationSuffix;
-    // let virualizationUrl2 = 'https://phantomjscloud.com/api/browser/v2/a-demo-key-with-low-quota-per-ip-address/?request={url:'+encodeURI('\"'+externalJbrowseUrl+'\"')+',renderType:"jpg"}';
 
     return (
       <div id="genomeViewer">
-        {/*<iframe id="genomeFrame" className={style.jbrowse} src={internalJbrowseUrl}/>*/}
+        {/*<iframe id="genomeFrame" className={style.jbrowse} src={externalJbrowseUrl}/>*/}
         <a href={externalJbrowseUrl.replace('overview.html','index.html')}>Genome Viewer<i className="fa fa-link" /> </a>
         <a href={externalJbrowseUrl}>Overview<i className="fa fa-link" /> </a>
         <br />
