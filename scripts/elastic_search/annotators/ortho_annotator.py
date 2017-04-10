c8744656b6c7df834d344186bcce2b91b5c89161
Chris Tabone <ctabone@morgan.harvard.edu>
2017-04-10 13:03:52 -0400

    AGR-120 Removed old ortho loader / annotator files.

--- scripts/elastic_search/annotators/ortho_annotator.py
+++
@@ -1,8 +0,0 @@
-class OrthoAnnotator:
-
-    @staticmethod
-    def attach_annotations(gene, ortho_dataset):
-    	gene_id = gene['primaryId']
-    	if gene_id in ortho_dataset:
-    		gene['Orthology'] = ortho_dataset[gene_id]
-        return gene
