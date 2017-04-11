ab4b2aeea51b60d1b8f61b5559f4186b7a308c0a
Sierra Moxon <staylor1@uoregon.edu>
2017-04-11 16:35:52 -0700

    adding in modifiers

--- indexer/src/loaders/disease_loader.py
+++ indexer/src/loaders/disease_loader.py
@@ -21,20 +21,10 @@ class DiseaseLoader:
             experimentalConditions = []
             objectRelationMap = {}
             evidenceList = []
-            #geneticModifiers = []
-            #experimentalConditionsText = []
+            geneticModifiers = []
             inferredFromGeneAssociations = []
             modifier = {}
 
-            # if 'experimentalConditions' in diseaseRecord:
-            #     for experimentalCondition in diseaseRecord['experimentalConditions']:
-            #         experimentalConditions.append({"zecoId": experimentalCondition.get('zecoId'),
-            #                                        "geneOntologyId": experimentalCondition.get('geneOntologyId'),
-            #                                        "ncbiTaxonId": experimentalCondition.get('ncbiTaxonID'),
-            #                                        "chebiOntologyId": experimentalCondition.get('chebiOntologyId'),
-            #                                        "anatomicalId": experimentalCondition.get('anatomicalId'),
-            #                                        "experimentalConditionIsStandard": experimentalCondition.get('conditiionIsStandard'),
-            #                                        "freeTextCondition": experimentalCondition.get('textCondition')})
             if 'evidence' in diseaseRecord:
                 for evidence in diseaseRecord['evidence']:
                     evidenceCode = evidence.get('evidenceCode')
@@ -42,11 +32,7 @@ class DiseaseLoader:
                     for pub in evidence['publications']:
                         pubMedId = pub.get('pubMedId')
                         publicationModId = pub.get('publicationModId')
-                        # print pubMedId
-                        # print publicationModId
-                        # print diseaseRecord['evidence']
                         pubs.append({'pubMedId': pubMedId, 'publicationModId': publicationModId})
-                        # print pubs
                     evidenceList.append({"pubs": pubs, "evidenceCode": evidenceCode})
 
             if 'objectRelation' in diseaseRecord:
@@ -66,31 +52,34 @@ class DiseaseLoader:
                                                    "experimentalConditionIsStandard": experimentalCondition.get(
                                                        'conditiionIsStandard'),
                                                    "freeTextCondition": experimentalCondition.get('textCondition')})
-            # if 'modifier' in diseaseRecord:
-            #     associationType = diseaseRecord['modifier']['associationType'].get('associationType')
-            #     if 'genetic' in diseaseRecord['modifier']['associationType']:
-            #         geneticModifiers = diseaseRecord['modifier']['associationType'].get('genetic')
-            #     if 'experimentalConditionsText' in diseaseRecord['modifier']['associationType']:
-            #         experimentalConditionsText = diseaseRecord['modifier']['associationType'].get("experimentalConditionsText")
-            #     modifierQualifier = diseaseRecord['modifier']['associationType'].get("qualifier")
-            #     modifier.append({"associationType": associationType,
-            #                      "geneticModifiers": geneticModifiers,
-            #                      "experimentalConditionsText": experimentalConditionsText,
-            #                      "modifierQualifier": modifierQualifier})
+            if 'modifier' in diseaseRecord:
+                associationType = diseaseRecord['modifier']['associationType']
+                print associationType
+                if 'genetic' in diseaseRecord['modifier']:
+                    for geneticModifier in diseaseRecord['modifier'].get('genetic'):
+                        print geneticModifier
+                        geneticModifier.append(diseaseRecord['modifier'].get('genetic'))
+                if 'experimentalConditionsText' in diseaseRecord['modifier']:
+                    experimentalConditionsText = diseaseRecord['modifier'].get('experimentalConditionsText')
+                modifierQualifier = diseaseRecord['modifier']['qualifier']
+                modifier = {"associationType": associationType,
+                            "geneticModifiers": geneticModifiers,
+                            "experimentalConditionsText": experimentalConditionsText,
+                            "modifierQualifier": modifierQualifier}
 
             disease_annots[diseaseRecord.get('objectId')] = {
                 "diseaseObjectId": diseaseRecord.get('objectId'),
-                #"diseaseObjectName": diseaseRecord.get('objectName'),
-                #"qualifier": diseaseRecord.get('qualifier'),
-                #"with": diseaseRecord.get('with'),
-                #"taxonId": diseaseRecord.get('taxonId'),
-                #"geneticSex": diseaseRecord.get('geneticSex'),
-                #"dataAssigned": diseaseRecord.get('dateAssigned'),
+                "diseaseObjectName": diseaseRecord.get('objectName'),
+                "qualifier": diseaseRecord.get('qualifier'),
+                "with": diseaseRecord.get('with'),
+                "taxonId": diseaseRecord.get('taxonId'),
+                "geneticSex": diseaseRecord.get('geneticSex'),
+                "dataAssigned": diseaseRecord.get('dateAssigned'),
                 "experimentalConditions": experimentalConditions,
                 "associationType": diseaseRecord.get('objectRelation').get('associationType'),
-                #"diseaseObjectType": diseaseRecord.get('objectRelation').get('objectType'),
-                #"evidenceList": evidenceList,
-                #"modifier": modifier,
+                "diseaseObjectType": diseaseRecord.get('objectRelation').get('objectType'),
+                "evidenceList": evidenceList,
+                "modifier": modifier,
                 "objectRelation": objectRelationMap,
                 "evidence": evidenceList,
                 "do_id": diseaseRecord.get('DOid'),
