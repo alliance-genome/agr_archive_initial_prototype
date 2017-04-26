8d04c2d42c558e2b2bd6214798d2c66c3bcec284
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-04-26 15:46:13 -0700

    fixing BGI loader to add urls, local ids and global ids

--- indexer/src/annotators/ortho_annotator.py
+++ indexer/src/annotators/ortho_annotator.py
@@ -1,11 +1,6 @@
 class OrthoAnnotator:
     @staticmethod
     def attach_annotations(gene, ortho_dataset):
-        if gene['taxonId'] == "NCBITaxon:10090" or "NCBITaxon:9606":
-            gene_id = gene['primaryId']
-        else:
-            global_id = gene['primaryId']
-            gene_id = global_id.split(":")[1]
         if gene_id in ortho_dataset:
             gene['Orthology'] = ortho_dataset[gene_id]
         return gene
