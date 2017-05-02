b6812cbd08777f9c13b4e2f246a7ec174c55856a
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-05-02 08:40:31 -0700

    fixing case sensitivity on file open

--- indexer/src/files/csv_file.py
+++ indexer/src/files/csv_file.py
@@ -9,7 +9,7 @@ class CSVFile:
 
     def get_data(self):
         print "Loading csv data from (" + self.filename + ") ..."
-        with codecs.open(self.filename, 'r', 'UTF-8') as f:
+        with codecs.open(self.filename, 'r', 'utf-8') as f:
             reader = csv.reader(CommentFile(f), delimiter='\t')
             rows = []
             for row in reader:
