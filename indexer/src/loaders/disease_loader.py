5a6c6873acc2deeb3cc03191ab3cced4bc56e4c2
Sierra Moxon <sierra-moxon@users.noreply.github.com>
2017-05-10 12:13:30 -0700

    AGR-392

--- indexer/src/loaders/disease_loader.py
+++ indexer/src/loaders/disease_loader.py
@@ -44,7 +44,7 @@ class DiseaseLoader:
 
             if 'objectRelation' in diseaseRecord:
                 diseaseObjectType = diseaseRecord['objectRelation'].get("objectType")
-                diseaseAssociationType = diseaseRecord['objectRelation'].get("association_type")
+                diseaseAssociationType = diseaseRecord['objectRelation'].get("associationType")
                 #for gene in diseaseRecord['objectRelation']['inferredGeneAssociation']:
                 #        inferredFromGeneAssociations.append(gene.get('primaryId'))
                 objectRelationMap = {"diseaseObjectType": diseaseObjectType, "diseaseAssociationType": diseaseAssociationType}
